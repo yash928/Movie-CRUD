df27c9b9078c0cee9400e2b92397454826a5b855
unknown <yash12tyagi@gmail.com>
2024-06-03 12:44:48 +0530

    feat: docker compose

--- Movie-CRUD-admin/internal/adapter/persistance/db/connect.go
+++ Movie-CRUD-admin/internal/adapter/persistance/db/connect.go
@@ -1,5 +1,19 @@
 package db
 
-func Connect() {
-	// sql.Open("",)
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"movie_crud/internal/config"
+)
+
+func Connect(dbConf config.DB) (*sql.DB, error) {
+
+	connstr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable", dbConf.Driver, dbConf.Username, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Database)
+	log.Println("Connecting db to :", connstr)
+	conn, err := sql.Open(dbConf.Driver, connstr)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
